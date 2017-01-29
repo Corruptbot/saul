4998d1d89a40ad05c5b1af93802e2bbd66071013
rubcuadra <rubcuadra@gmail.com>
2017-01-28 18:46:04 -0600

    removed payload

--- bot/views.py
+++ bot/views.py
@@ -126,11 +126,11 @@ def askAutoContext(message):
     sender = message['sender']['id']
     #post_facebook_message(sender, "Antes que nada recuerda que nadie puede hacerte bajar del coche y no permitas que te presionen con amenazas o solicitando dinero\n" )
     buttons = []
-    buttons.append( Button(type="postback",payload="velocidad",title='Velocidad') )
-    buttons.append( Button(type="postback",payload="celular",title='Celular') )
-    buttons.append( Button(type="postback",payload="semaforo",title='Semaforos') )
-    buttons.append( Button(type="postback",payload="vuelta",title='Vueltas prohibidas') )
-    buttons.append( Button(type="postback",payload="verificacion",title='Verificacion') )
-    buttons.append( Button(type="postback",payload="cinturon",title='Cinturon de seguridad') )
+    buttons.append( Button(type="postback",title='Velocidad') )
+    buttons.append( Button(type="postback",title='Celular') )
+    buttons.append( Button(type="postback",title='Semaforos') )
+    buttons.append( Button(type="postback",title='Vueltas prohibidas') )
+    buttons.append( Button(type="postback",title='Verificacion') )
+    buttons.append( Button(type="postback",title='Cinturon de seguridad') )
     bot.send_button_message(sender,"Antes que nada recuerda que nadie puede hacerte bajar del coche y no permitas que te presionen con amenazas o solicitando dinero",buttons)
 
