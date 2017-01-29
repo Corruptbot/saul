817d51d3712f9cef9c29f29dc48515dca94b3c79
rubcuadra <rubcuadra@gmail.com>
2017-01-29 11:06:46 -0600

    distractor

--- bot/reglamento.py
+++ bot/reglamento.py
@@ -1,4 +1,4 @@
-giro_indebido[] 
+giro_indebido = [] 
 giro_indebido[0]= '''
 Artículo 10: En las glorietas los coches que se encuentran circulando dentro de ellas tienen preferencia, asi como los vehículos que deseen salir de las mismas. (Fracción IX)
 Las vueltas continuas a la derecha e izquierda están prohibidas a menos que haya señalamiento que indique lo contrario y siempre dando preferencia a los peatones y autos que vienen sobre la calle a la que se incorporaran. (Fracción X)
@@ -7,4 +7,8 @@ giro_indebido[1]= '''
 Artículo 11: Está prohibido realizar movimientos o giros diferentes a los indicados por la señalización en las calles en carriles destinados para dar vuelta en cualquier sentido.(Fracción VII)
 También no está permitido dar vuelta en U, cerca de una vuelta o donde exista señalización que lo impida. (Fracción VIII)
 Queda prohibido interferir los carriles exclusivos de transporte público al dar vuelta a la izquierda, derecha o en “U”, así como cambiar de calle cuando existan señalizaciones que lo impidan. (Fracción X-a)
+'''
+
+distractor = [0] = '''
+
 '''
