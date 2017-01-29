2c44e521ff7dc7f49e05b127fd7627f060f628d4
rubcuadra <rubcuadra@gmail.com>
2017-01-28 21:32:26 -0600

    yeah

--- bot/urls.py
+++ bot/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import include, url
 from .views import BotView
 
 urlpatterns = [
-	url(r'^other/?$', BotView.as_view() ),
+	url(r'^7ceba258cd1fa6adaacd4a3541c299c8948d0261401ce4c271/?$', BotView.as_view() ),
 	]
