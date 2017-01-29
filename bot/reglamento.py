0d04bf6b50a02818c942202b7c038c8a9f0938a2
rubcuadra <rubcuadra@gmail.com>
2017-01-29 11:09:08 -0600

    append

--- bot/reglamento.py
+++ bot/reglamento.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 giro_indebido = [] 
-giro_indebido[0]= '''
+giro_indebido.append('''
 Artículo 10: En las glorietas los coches que se encuentran circulando dentro de ellas tienen preferencia, asi como los vehículos que deseen salir de las mismas. (Fracción IX)
 Las vueltas continuas a la derecha e izquierda están prohibidas a menos que haya señalamiento que indique lo contrario y siempre dando preferencia a los peatones y autos que vienen sobre la calle a la que se incorporaran. (Fracción X)
-'''
-giro_indebido[1]= '''
+''')
+giro_indebido.append('''
 Artículo 11: Está prohibido realizar movimientos o giros diferentes a los indicados por la señalización en las calles en carriles destinados para dar vuelta en cualquier sentido.(Fracción VII)
 También no está permitido dar vuelta en U, cerca de una vuelta o donde exista señalización que lo impida. (Fracción VIII)
 Queda prohibido interferir los carriles exclusivos de transporte público al dar vuelta a la izquierda, derecha o en “U”, así como cambiar de calle cuando existan señalizaciones que lo impidan. (Fracción X-a)
-'''
+''')
 
 distractor = []
-distractor = '''
+distractor.append('''
 Artículo 38: Se prohíbe utilizar o manejar cualquier aparato electrónico o de comunicación mientras el automóvil se encuentra en movimiento. Así como distractores como mapas o cualquier objeto que afecte la visibilidad y pueda ser un distractor. (Apartado II)
-Recuerda sólo puedes utilizar tu celular cuando el vehículo está detenido.'''
+Recuerda sólo puedes utilizar tu celular cuando el vehículo está detenido.''')
