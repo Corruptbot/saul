bfd80c57734d68b4ff2bd4fbaf2acd1a24e80e42
rubcuadra <rubcuadra@gmail.com>
2017-01-29 10:55:04 -0600

    load policias

--- test.py
+++ test.py
@@ -1,25 +1,5 @@
-from wit import Wit
-
-def send(request, response):
-    print 'Sending to user...', response['text']
-def my_action(request):
-    print 'Received from user...', request['text']
-
-actions = {
-    'send': send,
-    'my_action': my_action,
-}
-
-access_token = '5U7WOSQ7QB3OBE47OFRUHQDLRKH6PF7P'
-
-client = Wit(access_token=access_token, actions=actions)
-resp = client.message('la chota quiere que me baje del coche por pasarme un semaforo ayuda')
-#print str(resp)
-
-asText = ""
-for i in resp['entities']:
-	section = resp['entities'][i] #number, entitie, etc
-	for entitie in section:
-		asText+='%s %s% \n'%(entitie['confidence'],entitie['value'])
-		print entitie
-	
+with open('policias.bd') as f:
+	for line in f.readlines():
+		divided = line.split()
+		poli = PoliciaTransito(p_id=int(divided[0]),name=' '.join(divided[1:]))
+		poli.save()
