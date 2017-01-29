d2e08dcf6b616593b99fc56effa57c95611f6332
rubcuadra <rubcuadra@gmail.com>
2017-01-29 10:55:44 -0600

    load

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-with open('policias.bd') as f:
-	for line in f.readlines():
-		divided = line.split()
-		poli = PoliciaTransito(p_id=int(divided[0]),name=' '.join(divided[1:]))
-		poli.save()
+from bot.models import PoliciaTransito
+def load():
+	with open('policias.bd') as f:
+		for line in f.readlines():
+			divided = line.split()
+			poli = PoliciaTransito(p_id=int(divided[0]),name=' '.join(divided[1:]))
+			poli.save()
