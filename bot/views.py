54c00e0a3240f49be46d75658aa7e6ea350e59fa
rubcuadra <rubcuadra@gmail.com>
2017-01-29 12:47:32 -0600

    pus

--- bot/views.py
+++ bot/views.py
@@ -191,6 +191,7 @@ def askAutoContext(message):
     button = Button(type="postback",title='Pasarse alto',payload='alto')
     buttons.append(button)
     bot.send_button_message(sender,"Antes que nada recuerda que: El agente no puede exigirte que bajes de tu auto si la infracción que cometiste no es grave, no permitas que te presionen\n" ,buttons)
+    post_facebook_message(sender,"'Si el agente pide revisar e interior de tu auto o la cajuela, este sólo podrá hacerlo si tiene una orden judicia'")
     post_facebook_message(sender,"Si es algun otro podrias explicarmelo o ayudame a identificar al oficial que te detuvo enviandome su matricula?" )
 
 '''
